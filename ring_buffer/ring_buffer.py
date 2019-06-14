1c5b63baea7883671a5ece5e0d13cb1fba76c5ce
Julian Moreno <julmo611@gmail.com>
2019-06-14 10:23:22 -0700

    ring buffer Done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,21 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        if self.current < self.capacity:
+            self.storage[self.current] = item
+            self.current += 1
 
-  def get(self):
-    pass
+        if self.current == self.capacity:
+            self.current = 0
+
+    def get(self):
+        arr = []
+
+        for item in self.storage:
+            if item is not None:
+                arr.append(item)
+        return arr
